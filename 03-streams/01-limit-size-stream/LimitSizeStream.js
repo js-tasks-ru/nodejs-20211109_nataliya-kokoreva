4f73d6a741100d7892e554fc7c56a9d3f191858d
Nataliya Kokoreva <nmkokoreva@avito.ru>
2021-11-22 22:19:27 +0300

    add solution for 3-module 1-task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,16 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.currentDataLength = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.currentDataLength += encoding === 'buffer' ?
+      chunk.length : chunk.toString(encoding).length;
+    if (this.currentDataLength >= this.limit ) {
+      callback(new LimitExceededError(), chunk);
+    } else callback(null, chunk);
   }
 }
 
