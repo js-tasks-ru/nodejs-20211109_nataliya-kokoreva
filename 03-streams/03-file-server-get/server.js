f67137d5ddec403f6ac2a3d7cb2df20218e937a0
Nataliya Kokoreva <nmkokoreva@avito.ru>
2021-11-23 00:48:15 +0300

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,17 +1,39 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
+  const urlWrapper = new URL(req.url, `http://${ req.headers.host }`);
+  const pathname = urlWrapper.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const fileStream = fs.createReadStream(filepath);
+  fileStream
+      .on('error', function(err) {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not found');
+        } else {
+          send500(res);
+        }
+      })
+      .on('close', function() {
+        fileStream.destroy();
+      });
+
+  req.on('aborted', () => fileStream.destroy());
+
   switch (req.method) {
     case 'GET':
+      if (req.url === `/${ pathname.split('/')[0] }`) {
+        fileStream.pipe(res).on('error', send500(res));
+      } else {
+        res.statusCode = 400;
+        res.end('Bad request');
+      }
 
       break;
 
@@ -20,5 +42,10 @@ server.on('request', (req, res) => {
       res.end('Not implemented');
   }
 });
+
+send500 = (res) => () => {
+  res.statusCode = 500;
+  res.end('Server error');
+};
 
 module.exports = server;
