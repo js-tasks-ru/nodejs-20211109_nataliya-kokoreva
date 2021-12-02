bea5e9cd0012ae4555ba324a62cd2a8356b31439
Nataliya Kokoreva <nmkokoreva@avito.ru>
2021-12-03 00:42:54 +0300

    add solution for 5-module 1-task

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,38 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let clients = {};
+let timerId;
+
 router.get('/subscribe', async (ctx, next) => {
+  clients[ctx.originalUrl] = ctx;
+
+  await new Promise((resolve) => {
+    timerId = setInterval(()=> {
+      if (Object.keys(clients).length === 0) {
+        resolve();
+      }
+    }, 500);
+  });
+
+  ctx.req.on('aborted', () => {
+    clients.splice(clients.indexOf(ctx), 1),
+    clearInterval(timerId);
+  });
+
+  next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+  ctx.response.status = 200;
+  if (message !== '') {
+    Object.values(clients).forEach((clientCtx) => {
+      clientCtx.body = message;
+    });
+
+    clients = {};
+  } else next();
 });
 
 app.use(router.routes());
